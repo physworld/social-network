a59ef298899b86fbc88680a2ee3399ab1ba3188f
vladtkalenko <vladtkalenko@gmail.com>
2020-07-22 01:30:45 +0300

    renamed repository

---
+++ social-network/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const app = express();
+
+/******************************
+ MIDDLEWARES
+ */
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); //All css and html in public folder
+
+/**************************
+ *
+ * ROUTES
+ */
+
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
