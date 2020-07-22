4cf2232480bbc291c2ae28d3c2a717f1f90c698d
vladtkalenko <vladtkalenko@gmail.com>
2020-07-23 02:02:07 +0300

    deleted unnecessary functions from errorController

--- social-network/controllers/errorController.js
+++ social-network/controllers/errorController.js
@@ -1,23 +1,3 @@
-const AppError = require('./../utils/appError');
-
-const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}: ${err.value}.`;
-    return new AppError(message, 400);
-};
-
-const handleDuplicateErrorDB = err => {
-    const value = err.keyValue.name;
-    console.log(value);
-    const message = `Duplicate field value: ${value}. Please use another value!`;
-    return new AppError(message, 400);
-};
-
-const handleValidationErrorDB = err => {
-    const errors = Object.values(err.errors).map(el => el.message);
-
-    const message = `Invalid input data. ${errors.join('. ')}`;
-    return new AppError(message, 400);
-};
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -49,19 +29,12 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-    // console.log(err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
-    } else if (process.env.NODE_ENV === 'production') {
-        let error = Object.assign(err);
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
-        if (error.code === 11000) error = handleDuplicateErrorDB(error);
-        if (error.name = 'ValidationError') error = handleValidationErrorDB(error);
-
-        sendErrorProd(error, res);
-    }
+    } else if (process.env.NODE_ENV === 'production')
+        sendErrorProd(err, res);
 };
--- social-network/utils/catchAsync.js
+++ social-network/utils/catchAsync.js
@@ -1,3 +1,4 @@
+//TO CATCH ERRORS IN ASYNC FUNCTIONS
 module.exports = fn => {
     return (req, res, next) => {
         fn(req, res, next).catch(next);
